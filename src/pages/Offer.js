76d9d2455f6576933ec65e1dc9b6495f332b61eb
vipin kumar <vipinatraura@gmail.com>
2022-07-13 12:53:59 +0530

    update carousel

--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -5,13 +5,12 @@ import Footer from "../components/Footer";
 import "react-multi-carousel/lib/styles.css";
 import axios from "axios";
 import { BASE_URL } from "../Api_connection/config";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Offer = () => {
 
   const [offers, setOffers] = useState([])
-  const [ind, setInd] = useState(0)
+
   const getOffers = async () => {
     try {
       const data = await axios.post(`${BASE_URL}/offers`)
@@ -20,6 +19,7 @@ const Offer = () => {
     } catch (error) {
     }
   }
+
   useEffect(() => {
     getOffers()
   }, [])
@@ -43,96 +43,24 @@ const Offer = () => {
                 </div>
                 <div className="container">
                   <div className="shadow mt-3">
-                    <Carousel
-                    showThumbs={false}
-                    infiniteLoop={true}
-                    autoPlay={true}
-                    swipeable={true}
-                    >
+                    <Carousel fade>
                       {
                         offers.map((element, index) => {
                           return (
-                            <div>
-                              <img src={`http://localhost:3001${element.image}`} />
-                              {/* <p className="legend">Legend 1</p> */}
-                            </div>
+                            <Carousel.Item key={index}>
+                              <img
+                                style={{ height: "471px", width: "1000px" }}
+                                className="d-block w-100"
+                                src={`http://localhost:3001${element.image}`}
+                                alt={index+1}
+                              />
+                            </Carousel.Item>
                           )
                         })
                       }
-                      {/* <div>
-                        <img src="assets/1.jpeg" />
-                        <p className="legend">Legend 1</p>
-                      </div>
-                      <div>
-                        <img src="assets/2.jpeg" />
-                        <p className="legend">Legend 2</p>
-                      </div>
-                      <div>
-                        <img src="assets/3.jpeg" />
-                        <p className="legend">Legend 3</p>
-                      </div> */}
                     </Carousel>
-
-                    {/* <div id="carouselExFade" class="carousel slide carousel-fade" data-ride="carousel" data-interval="2000">
-
-                      < ol class="carousel-indicators mb-n5">
-                        <li data-bs-target="#carouselExFade" data-bs-slide-to="0" class="active"></li>
-                        <li data-bs-target="#carouselExFade" data-bs-slide-to="1"></li>
-                        <li data-bs-target="#carouselExFade" data-bs-slide-to="2"></li>
-                      </ol>
-                      <div class="carousel-inner">
-                        {
-
-                        }
-                        <div class="carousel-item active">
-                          <img class="d-block w-100" src="images/offer/offer1.jpg" alt="First Offer" />
-                        </div>
-
-                        <div class="carousel-item">
-                          <img class="d-block w-100" src="images/offer/offer5.jpg" alt="Third Offer" />
-                        </div>
-                        <div class="carousel-item">
-                          <img class="d-block w-100" src="images/offer/offer3.jpg" alt="Third Offer" />
-                        </div>
-                      </div>
-
-                      <a class="carousel-control-prev" href="#carouselExFade" role="button" data-bs-slide="prev"> <span
-                        class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="visually-hidden">Previous</span>
-                      </a> <a class="carousel-control-next" href="#carouselExFade" role="button" data-bs-slide="next"> <span
-                        class="carousel-control-next-icon" aria-hidden="true"></span> <span class="visually-hidden">Next</span> </a>
-                    </div> */}
-                    {/* <Slide>
-                  {i?.map((slideImage, index) => {
-                    return (
-                      <div className="each-slide border border-" key={index}>
-                        <div
-                          style={{
-                            backgroundImage: `url(http://localhost:3001${slideImage.banner})`,
-                            // height: "250px",
-                            width: "100%",
-                            backgroundSize: "cover",
-                            backgroundRepeat: "no-repeat",
-                            backgroundAttachment: "fixed",
-                          }}
-                        >
-                          <span
-                            style={{
-                              height: "250px",
-                              display: "inline-block",
-                             // marginTop: "20px",
-                            }}
-                          >
-                            {slideImage.caption}
-                          </span>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </Slide> */}
                   </div>
                 </div>
-
-
               </div>
             </div>
           </div>
