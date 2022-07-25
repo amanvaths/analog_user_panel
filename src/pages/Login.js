b17cd97ffcf2e3f0f7b28cab079846e88ff085d6
swatiyad <swati.nadcabtechnology@gmail.com>
2022-07-25 17:12:06 +0530

    background img changed for signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -262,9 +262,9 @@ const Login = (props) => {
                         width={320}
                         height={160}
                         visible={true}
-                        onSuccess={() => alert('success')}
-                        onFail={() => alert('fail')}
-                        onRefresh={() => alert('refresh')}
+                        onSuccess={() => alert('Successfully Matched !')}
+                        onFail={() => alert('You are Failed ! Please Try Again')}
+                        onRefresh={() => alert('Refreshed successfully ')}
                       />
                     {/* </div> */}
                      
